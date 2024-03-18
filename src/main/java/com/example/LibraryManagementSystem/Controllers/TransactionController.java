7effb07dd2935d87ca77cf790776dfa1a184355b
Piyush Verma <piyushvrm265@gmail.com>
2024-03-18 21:59:39 +0530

    Added issue and return book APIs

--- src/main/java/com/example/LibraryManagementSystem/Controllers/TransactionController.java
+++ src/main/java/com/example/LibraryManagementSystem/Controllers/TransactionController.java
@@ -9,6 +9,24 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("transaction")
 public class TransactionController {
+    @Autowired
+    private TransactionService transactionService;
 
+    @PostMapping("/issueBook")
+    public String issueBook(@RequestBody IssueBookRequest issueBookRequest){
+        try{
+            String result = transactionService.issueBook(issueBookRequest);
+            return result;
+        }
+        catch(Exception e){
+            return e.getMessage();
+        }
+    }
+
+    @PostMapping("/returnBook")
+    public String returnBook(@RequestBody ReturnBookRequest returnBookRequest){
+        String result = transactionService.returnBook(returnBookRequest);
+        return result;
+    }
 
 }
--- src/main/java/com/example/LibraryManagementSystem/Services/TransactionService.java
+++ src/main/java/com/example/LibraryManagementSystem/Services/TransactionService.java
@@ -21,7 +21,72 @@ import java.util.Optional;
 @Service
 public class TransactionService {
 
+    @Autowired
+    private TransactionRepository transactionRepository;
 
+    @Autowired
+    private LibraryCardRepository cardRepository;
 
+    @Autowired
+    private BookRepository bookRepository;
+
+    public String issueBook(IssueBookRequest issueBookRequest) throws Exception{
+        LibraryCard card = cardRepository.findById(issueBookRequest.getCardId()).get();
+        Book book = bookRepository.findById(issueBookRequest.getBookId()).get();
+
+        System.out.println("iN TRANSACTION");
+        LocalDate date = LocalDate.parse("2024-01-05");
+
+        Student student = card.getStudent();
+
+        if(!book.getIsAvailable()){
+            throw new Exception("Book unavailable");
+
+        }
+
+        if(card.getNoOfBooksIssued() >= 3){
+            throw new Exception("Maximum number of books already issued to this card");
+        }
+
+        Transactions transaction = Transactions.builder()
+                .book(book)
+                .transactionType(TransactionType.ISSUED)
+                .transactionStatus(TransactionStatus.SUCCESS)
+                .libraryCard(card)
+                .build();
+
+        book.setIsAvailable(Boolean.FALSE);
+
+        book.getTransactionList().add(transaction);
+        card.getTransactionList().add(transaction);
+
+        transaction = transactionRepository.save(transaction);
+
+        return "Transaction have been saved to the db with Id: "+transaction.getTransactionId();
+
+    }
+
+    public String returnBook(ReturnBookRequest returnBookRequest){
+        LibraryCard card = cardRepository.findById(returnBookRequest.getCardId()).get();
+        Book book = bookRepository.findById(returnBookRequest.getBookId()).get();
+
+        card.setNoOfBooksIssued(card.getNoOfBooksIssued()-1);
+        book.setIsAvailable(Boolean.TRUE);
+
+        Transactions transaction = Transactions.builder()
+                .book(book)
+                .libraryCard(card)
+                .transactionType(TransactionType.RETURN)
+                .transactionStatus(TransactionStatus.SUCCESS)
+                .build();
+
+        card.getTransactionList().add(transaction);
+        book.getTransactionList().add(transaction);
+
+        transaction = transactionRepository.save(transaction);
+
+        return "Book "+book.getBookName()+" with Id: "+book.getBookId()+" has been successfully returned.";
+
+    }
 
 }
